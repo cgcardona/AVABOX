f44a9bc2ac1056217693b12d70df3015211b43e0
Gabriel Cardona <gabriel.earth.engineering@gmail.com>
2020-05-11 10:46:57 -0700

    Adding const and types.

--- examples/create_token.ts
+++ examples/create_token.ts
@@ -7,8 +7,8 @@ import { AVMKeyChain, AVMKeyPair } from 'slopes'
 import * as slopes from 'slopes'
 import { Config } from '../src/interfaces/config_interface'
 
-let main = async (): Promise<any> => {
-  let config: Config = {
+const main = async (): Promise<any> => {
+  const config: Config = {
     fullNodeHost: 'ip.of.full.node',
     fullNodePort: 9650,
     fullNodeProtocol: 'http',
@@ -18,35 +18,33 @@ let main = async (): Promise<any> => {
   const avabox: AVABOX = new AVABOX(config)
   const Wallet: Wallet = avabox.Wallet
   const avm: AVMAPI = Wallet.avm
-  let myKeychain = avm.keyChain();
+  const myKeychain: AVMKeyChain = avm.keyChain();
 
-  let newAddress1: Buffer = myKeychain.makeKey()
-  let keypair1 = myKeychain.getKey(newAddress1); //returns the keypair class
-  let privkey1 = keypair1.getPrivateKeyString()
+  const newAddress1: Buffer = myKeychain.makeKey()
+  const keypair1: AVMKeyPair = myKeychain.getKey(newAddress1); //returns the keypair class
+  const privkey1: string = keypair1.getPrivateKeyString()
   console.log(`privkey1: ${privkey1}`)
 
-  let mypk1 = Buffer.from('')
-  let successful1 = keypair1.importKey(mypk1)
+  const mypk1: Buffer = Buffer.from('')
+  const successful1: boolean = keypair1.importKey(mypk1)
   console.log(`successful1: ${successful1}`)
 
-  let newAddress2: Buffer = myKeychain.makeKey()
-  let keypair2 = myKeychain.getKey(newAddress2); //returns the keypair class
-  let privkey2 = keypair2.getPrivateKeyString()
+  const newAddress2: Buffer = myKeychain.makeKey()
+  const keypair2: AVMKeyPair = myKeychain.getKey(newAddress2); //returns the keypair class
+  const privkey2: string = keypair2.getPrivateKeyString()
   console.log(`privkey2: ${privkey2}`)
-  let mypk2 = Buffer.from('')
-  let successful2 = keypair2.importKey(mypk2)
+  const mypk2: Buffer = Buffer.from('')
+  const successful2: boolean = keypair2.importKey(mypk2)
   console.log(`successful2: ${successful2}`)
 
-  let newAddress3: Buffer = myKeychain.makeKey()
-  let keypair3 = myKeychain.getKey(newAddress3); //returns the keypair class
-  let privkey3 = keypair3.getPrivateKeyString()
+  const newAddress3: Buffer = myKeychain.makeKey()
+  const keypair3: AVMKeyPair = myKeychain.getKey(newAddress3); //returns the keypair class
+  const privkey3: string = keypair3.getPrivateKeyString()
   console.log(`privkey3: ${privkey3}`)
-  let mypk3 = Buffer.from('')
-  let successful3 = keypair3.importKey(mypk3)
+  const mypk3: Buffer = Buffer.from('')
+  const successful3: boolean = keypair3.importKey(mypk3)
   console.log(`successful3: ${successful3}`)
-
   console.log(avm.keyChain().getAddressStrings())
-
 
   const fee: BN = new BN(0)
   const name: string = 'Hello EARTH'
@@ -59,27 +57,26 @@ let main = async (): Promise<any> => {
   const secpbase1: slopes.SecpOutBase = new slopes.SecpOutBase(new BN(400), addresses);
   const secpbase2: slopes.SecpOutBase = new slopes.SecpOutBase(new BN(500), [addresses[1]]);
   const secpbase3: slopes.SecpOutBase = new slopes.SecpOutBase(new BN(600), [addresses[1], addresses[2]]);
-  // console.log(secpbase1)
 
   // Populate the initialState array
   // The AVM needs to know what type of output is produced. 
   // The constant slopes.AVMConstants.SECPFXID is the correct output.
   // It specifies that we are using a secp256k1 signature scheme for this output.
-  let initialState: slopes.InitialStates = new slopes.InitialStates();
+  const initialState: slopes.InitialStates = new slopes.InitialStates();
   initialState.addOutput(secpbase1, slopes.AVMConstants.SECPFXID);
   initialState.addOutput(secpbase2, slopes.AVMConstants.SECPFXID);
   initialState.addOutput(secpbase3, slopes.AVMConstants.SECPFXID);
 
   // Fetch the UTXOSet for our addresses
-  let utxos = await avm.getUTXOs(addresses);
+  const utxos: slopes.UTXOSet = await avm.getUTXOs(addresses);
 
   // Make an unsigned Create Asset transaction from the data compiled earlier
-  let unsigned = await avm.makeCreateAssetTx(utxos, fee, addresses, initialState, name, symbol, denomination);
+  const unsigned: slopes.TxCreateAsset = await avm.makeCreateAssetTx(utxos, fee, addresses, initialState, name, symbol, denomination);
 
-  let signed = avm.keyChain().signTx(unsigned); //returns a Tx class
-  let txid = await avm.issueTx(signed); //returns an AVA serialized string for the TxID
+  const signed: slopes.Tx = avm.keyChain().signTx(unsigned); //returns a Tx class
+  const txid: string = await avm.issueTx(signed); //returns an AVA serialized string for the TxID
   console.log(`TXID: ${txid}`)
-  let status = await avm.getTxStatus(txid);
+  const status: string = await avm.getTxStatus(txid);
   console.log(`STATUS: ${status}`)
 }
 
