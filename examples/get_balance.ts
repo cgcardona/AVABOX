28a6b3543f20178bcae91175eb155f355d51c40e
Gabriel Cardona <gabriel.earth.engineering@gmail.com>
2020-05-10 22:02:01 -0700

    Add txid of ERTH token.

--- examples/get_balance.ts
+++ examples/get_balance.ts
@@ -1,17 +1,18 @@
-import { AVABOX } from "../src/AVABOX"
+import { AVABOX } from '../src/AVABOX'
 
 const main = async (): Promise<any> => {
   const config = {
-    fullNodeHost: "ip.address.of.node",
+    fullNodeHost: 'ip.of.full.node',
     fullNodePort: 9650,
-    fullNodeProtocol: "http",
+    fullNodeProtocol: 'http',
     networkId: 12345
 
   }
   const avabox: AVABOX = new AVABOX(config)
   const XChain = avabox.XChain
-  const address: string =  "X-GMQasdep6PraTrSfSbggcZFKJ3WsHYT17"
-  const balance: Promise<number> = await XChain.getBalance(address)
+  const address: string =  'X-GzNySBE2zK7cLoGhFGUZoZg57x689L6J7'
+  const assetId: string = '2BKo2z3qnMhUNFdD2nkMMGT4iV5QCZwRuBeegyXejbhG1JCp8j'
+  const balance: Promise<number> = await XChain.getBalance(address, assetId)
   console.log(balance)
 }
 
